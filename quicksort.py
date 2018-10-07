83fcc3dcf46dfa1de66bb67d8b3ee92006f67b72
Tototyr <mysilia46@gmail.com>
2018-10-07 17:43:18 +0600

    first commit

---
+++ quicksort.py
@@ -0,0 +1,10 @@
+def quicksort(arr):
+    if not arr:
+        return []
+    else:
+        return quicksort([x for x in arr if x < arr[0]]) \
+        + [x for x in arr if x == arr[0]] \
+        + quicksort([x for x in arr if x > arr[0]])
+
+
+print(quicksort([10,123,512,552,66]))
